227d1d6ee919fd75f56411451270aec3ee81164c
Quico Moya <me@qmoya.com>
2023-02-07 12:42:49 +0100

    Rename package to prevent collision

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-	name: "FirebaseAnalytics",
+	name: "SwiftFirebaseAnalytics",
 	platforms: [.iOS(.v15), .macOS(.v13)],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to
