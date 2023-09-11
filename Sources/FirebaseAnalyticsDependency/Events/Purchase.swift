09d3e24d79ad31fcab5ed26c1e947b5659957f06
qmoya <qmoya@users.noreply.github.com>
2023-09-11 20:07:34 +0000

    Run swift-format

--- Sources/FirebaseAnalyticsDependency/Events/Purchase.swift
+++ Sources/FirebaseAnalyticsDependency/Events/Purchase.swift
@@ -2,7 +2,18 @@ import Foundation
 
 public extension Event {
 	struct Purchase: Equatable {
-		public init(affiliation: String? = nil, coupon: String? = nil, value: Money? = nil, endDate: Date? = nil, itemID: String? = nil, items: [Item], shipping: Double, startDate: Date? = nil, tax: Double? = nil, transactionID: String? = nil) {
+		public init(
+			affiliation: String? = nil,
+			coupon: String? = nil,
+			value: Money? = nil,
+			endDate: Date? = nil,
+			itemID: String? = nil,
+			items: [Item],
+			shipping: Double,
+			startDate: Date? = nil,
+			tax: Double? = nil,
+			transactionID: String? = nil
+		) {
 			self.affiliation = affiliation
 			self.coupon = coupon
 			self.value = value
@@ -14,7 +25,7 @@ public extension Event {
 			self.tax = tax
 			self.transactionID = transactionID
 		}
-		
+
 		public let affiliation: String?
 		public let coupon: String?
 		public let value: Money?
