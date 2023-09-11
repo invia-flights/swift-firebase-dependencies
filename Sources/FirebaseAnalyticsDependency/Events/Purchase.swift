ed6f5650d5ea1c63d033530becd1150742f8a9ef
Quico Moya <me@qmoya.com>
2023-09-11 22:06:20 +0200

    Add initializer

--- Sources/FirebaseAnalyticsDependency/Events/Purchase.swift
+++ Sources/FirebaseAnalyticsDependency/Events/Purchase.swift
@@ -2,6 +2,19 @@ import Foundation
 
 public extension Event {
 	struct Purchase: Equatable {
+		public init(affiliation: String? = nil, coupon: String? = nil, value: Money? = nil, endDate: Date? = nil, itemID: String? = nil, items: [Item], shipping: Double, startDate: Date? = nil, tax: Double? = nil, transactionID: String? = nil) {
+			self.affiliation = affiliation
+			self.coupon = coupon
+			self.value = value
+			self.endDate = endDate
+			self.itemID = itemID
+			self.items = items
+			self.shipping = shipping
+			self.startDate = startDate
+			self.tax = tax
+			self.transactionID = transactionID
+		}
+		
 		public let affiliation: String?
 		public let coupon: String?
 		public let value: Money?
