d43c9097cd8a7d6198c93d99dc149f6a1cdba1cc
Quico Moya <me@qmoya.com>
2024-03-11 17:51:10 +0100

    Update dependency

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -27,7 +27,7 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.15.0"),
+		.package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.22.1"),
 		.package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.1.4"),
 	],
 	targets: [
