df9d7dafaa97553a15880aff57ef2972b56802cf
Quico Moya <me@qmoya.com>
2024-08-29 19:38:57 +0200

    Rename package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-	name: "FirebaseDependencies",
+	name: "swift-firebase-dependencies",
 	platforms: [.iOS(.v15), .macOS(.v12)],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to
